54eabb40f1dc4d73290dfc9c12222c42cfa95ca4
Steve Rude <steve@slantview.com>
2012-01-03 02:28:36 -0800

    update drush

--- drush/providers/command.rb
+++ drush/providers/command.rb
@@ -1,30 +1,33 @@
 action :run do
-  drush = {}
-  drush.cmd = "drush #{new_resource.command} "
-  drush.args = new_resource.args || []
-  drush.options = new_resource.options || []
+  drush_cmd = "drush #{new_resource.command} "
+  drush_args = new_resource.args || []
+  drush_options = new_resource.options || []
   if new_resource.site_alias
-    drush.cmd += "@#{new_resource.site_alias} "
+    drush_cmd += "@#{new_resource.site_alias} "
   end
   if new_resource.site_uri
-    drush.options.push("--uri=#{new_resource.site_uri}")
+    drush_options.push("--uri=#{new_resource.site_uri}")
   end
   if new_resource.site_dir
-    drush.options.push("--destination=#{new_resource.site_dir}")
+    drush_options.push("--destination=#{new_resource.site_dir}")
   end
   if new_resource.quiet
-    drush.options.push("-q")
+    drush_options.push("-q")
   end
   if new_resource.default_yes
-    drush.options.push("-y")
+    drush_options.push("-y")
   end
   if new_resource.args
-    drush.cmd += "#{new_resource.args.join(' ')} "
+    new_resource.args.each do |arg|
+      drush_args.push(arg)
+    end
   end
   if new_resource.options
-    drush.cmd += "#{new_resource.options.join(' ')} "
+    drush_cmd += "#{new_resource.options.join(' ')} "
   end
-  execute "#{drush.cmd}" do
-    command "#{drush.cmd}"
+  args = drush_args.join(" ")
+  options = drush_options.join(" ")
+  execute "#{drush_cmd} #{args} #{options}" do
+    command "#{drush_cmd}"
   end
 end
