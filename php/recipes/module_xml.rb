2304a5d25fa27967f0741b6c9c28fd8a09a04222
Steve Rude <steve@slantview.com>
2012-04-12 21:06:48 -0700

    add php-xml for centos

---
+++ php/recipes/module_xml.rb
@@ -0,0 +1,27 @@
+#
+# Author::  Steve Rude (<steve@slantview.com>)
+# Cookbook Name:: php
+# Recipe:: module_xml
+#
+# Copyright 2012 Slantview Media.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+pkg = value_for_platform(
+    [ "centos", "redhat", "fedora" ] => {"default" => "php-xml"}
+  )
+
+package pkg do
+  action :install
+end
